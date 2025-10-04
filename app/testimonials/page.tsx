b515aab4536ba3f574b64c6cb43047ec5db1f60f
nicktsan <nicktsang.manhay@gmail.com>
2025-10-04 11:47:09 -0700

    add testimonials page

---
+++ app/testimonials/page.tsx
@@ -0,0 +1,12 @@
+export default function Testimonials() {
+  return (
+    <main className="flex min-h-screen flex-col items-center py-24 px-8 gap-y-12 max-w-screen-md mx-auto">
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1 className="text-4xl font-bold">
+          Testimonials
+        </h1>
+      </div>
+
+    </main>
+  );
+}

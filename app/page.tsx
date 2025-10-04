d60e474ac3d95fb7197d6a02442365e5fc4bb133
nicktsan <nicktsang.manhay@gmail.com>
2025-10-04 11:41:44 -0700

    fix navbar and main page formatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import TestonomialForm from "@/components/form/testinomial-form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-y-12 max-w-screen-md mx-auto">
+    <main className="flex min-h-screen flex-col items-center py-24 px-8 gap-y-12 max-w-screen-md mx-auto">
       <div className="flex flex-col items-center justify-center text-center">
         <h1 className="text-4xl font-bold">
           Welcome to Club Freedom Testomonial

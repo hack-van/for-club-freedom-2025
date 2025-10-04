32dc4f926cc946491910a7beb02f1a4e8e19a39d
Viet Hung Nguyen <vhn@sfu.ca>
2025-10-04 12:39:17 -0700

    Add button

--- app/testimonials/[id]/page.tsx
+++ app/testimonials/[id]/page.tsx
@@ -1,5 +1,7 @@
 import TestimonialDetail from "@/components/testimonial-detail";
+import { Button } from "@/components/ui/button";
 import { Id } from "@/convex/_generated/dataModel";
+import Link from "next/dist/client/link";
 
 export default async function TestimonialPage({
   params,
@@ -8,7 +10,12 @@ export default async function TestimonialPage({
 }) {
   const { id } = await params;
   return (
-    <div className="max-w-screen-lg mx-auto py-24 px-8">
+    <div className="max-w-screen-lg mx-auto py-24 px-8 space-y-4">
+      <div>
+        <Button asChild>
+          <Link href="/">Make new record</Link>
+        </Button>
+      </div>
       <TestimonialDetail id={id as Id<"testimonials">} />
     </div>
   );

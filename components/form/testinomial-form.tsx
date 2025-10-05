3061fefdc3d083f59daecb39beba05a55cd51c13
Course Test <Fredopayn1119@cs.sfu.ca>
2025-10-04 20:50:58 -0700

    Reflected changes from modified database schema

--- components/form/testinomial-form.tsx
+++ components/form/testinomial-form.tsx
@@ -43,7 +43,7 @@ const formSchema = z
   .object({
     name: z.string().min(2, "Name must be at least 2 characters"),
     email: z.email("Please enter a valid email address"),
-    audioFile: z
+    mediaFile: z
       .file({ error: "Please record your audio testimonial" })
       .optional(),
     writtenText: z.string(),
@@ -53,8 +53,8 @@ const formSchema = z
   })
   .superRefine((data, ctx) => {
     if (
-      (!data.writtenText || data.audioFile) &&
-      (data.writtenText || !data.audioFile)
+      (!data.writtenText || data.mediaFile) &&
+      (data.writtenText || !data.mediaFile)
     ) {
       ctx.addIssue({
         code: "custom",
@@ -64,7 +64,7 @@ const formSchema = z
       ctx.addIssue({
         code: "custom",
         message: "Please provide an audio testimonial",
-        path: ["audioFile"],
+        path: ["mediaFile"],
       });
     }
   });
@@ -83,7 +83,7 @@ export default function TestimonialForm() {
 
   const handleTabChange = (value: string) => {
     setTabValue(value);
-    form.resetField("audioFile");
+    form.resetField("mediaFile");
     form.resetField("writtenText");
   };
 
@@ -106,18 +106,27 @@ export default function TestimonialForm() {
 
     try {
       let storageId: string | undefined = undefined;
-      if (values.audioFile) {
-        storageId = await uploadAudioFile(values.audioFile);
+      let media_type = "text"
+      if (values.mediaFile) {
+        storageId = await uploadAudioFile(values.mediaFile);
         if (!storageId) {
           throw new Error("Failed to upload audio file");
         }
+        if (values.mediaFile.type.startsWith("audio")) {
+          media_type = "audio"
+        }
+        else if (values.mediaFile.type.startsWith("video")) {
+          media_type = "video"
+        }
       }
+
 
       // Step 3: Save testimonial data with storage ID
       const id = await postTestimonial({
         name: values.name,
         email: values.email,
-        audio: storageId as any,
+        media_id: storageId as any,
+        media_type: media_type,
         text: values.writtenText,
       });
 
@@ -192,16 +201,16 @@ export default function TestimonialForm() {
             <TabsContent value="audio">
               <FormField
                 control={form.control}
-                name="audioFile"
+                name="mediaFile"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Audio Testimonial</FormLabel>
                     <FormControl>
                       <AudioRecorder
-                        onRecordingComplete={(audioFile) => {
+                        onRecordingComplete={(mediaFile) => {
                           // Update the form
-                          field.onChange(audioFile);
-                          console.log("Recorded audio file:", audioFile);
+                          field.onChange(mediaFile);
+                          console.log("Recorded audio file:", mediaFile);
                         }}
                       />
                     </FormControl>

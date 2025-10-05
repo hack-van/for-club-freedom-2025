c55ea41dad86b6b54b4bd131a2bdbe198cb0d47b
Viet Hung Nguyen <vhn@sfu.ca>
2025-10-05 14:17:03 -0700

    Navbar link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
-import Image from "next/image"
 
 export default function Navbar() {
   return (
     <header className="border-b px-4 md:px-6">
-      <div className="flex h-16 items-center justify-center">
-        <img className="h-10" src="/city_reach_logo.svg" alt="city-reach-logo"></img>
-      </div>
+      <Link href="/" className="flex h-16 items-center justify-center">
+        <img
+          className="h-10"
+          src="/city_reach_logo.svg"
+          alt="city-reach-logo"
+        ></img>
+      </Link>
     </header>
   );
 }

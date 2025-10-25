be4a32d5f1864246cb29b3d0f3efc159b44ca312
Viet Hung Nguyen <vhn@sfu.ca>
2025-10-25 16:51:52 -0700

    Show elapsed time when recording video

--- components/video-recorder.tsx
+++ components/video-recorder.tsx
@@ -3,12 +3,17 @@
 import { ReactMediaRecorder } from "react-media-recorder";
 import { Button } from "./ui/button";
 import { Square, Video } from "lucide-react";
+import { useRef, useState } from "react";
+import TimeElapsed from "./time-elapsed";
 
 type Props = {
   onRecordingComplete: (videoFile?: File) => void;
 };
 
 export default function VideoRecorder({ onRecordingComplete }: Props) {
+  const [elapsed, setElapsed] = useState(0);
+  const intervalRef = useRef<NodeJS.Timeout>(null);
+
   return (
     <ReactMediaRecorder
       video
@@ -16,7 +21,17 @@ export default function VideoRecorder({ onRecordingComplete }: Props) {
       blobPropertyBag={{
         type: "video/mp4",
       }}
+      onStart={() => {
+        setElapsed(0);
+        intervalRef.current = setInterval(() => {
+          setElapsed((prev) => prev + 1);
+        }, 1000);
+      }}
       onStop={(_, blob) => {
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null;
+        }
         const videoFile = new File(
           [blob],
           `video-recording-${Date.now()}.mp4`,
@@ -35,22 +50,29 @@ export default function VideoRecorder({ onRecordingComplete }: Props) {
         previewStream,
       }) => {
         const isRecording = status === "recording";
+        console.log("Rendering VideoRecorder, status:", status);
 
         return (
           <div className="flex flex-col p-4 border items-center rounded-lg gap-4 w-full">
             {/* Video Preview */}
             {isRecording && previewStream && (
-              <video
-                autoPlay
-                ref={(ref) => {
-                  if (ref) {
-                    ref.srcObject = previewStream;
-                  }
-                }}
-                muted
-                playsInline
-                className="w-full"
-              />
+              <div className="relative">
+                <TimeElapsed
+                  seconds={elapsed}
+                  className="absolute top-2 left-2"
+                />
+                <video
+                  autoPlay
+                  ref={(ref) => {
+                    if (ref) {
+                      ref.srcObject = previewStream;
+                    }
+                  }}
+                  muted
+                  playsInline
+                  className="w-full"
+                />
+              </div>
             )}
 
             {/* Video Playback */}

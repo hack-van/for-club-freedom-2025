d14b1783269b0775f9ed0d044b6eece8fdd743e1
Viet Hung Nguyen <vhn@sfu.ca>
2025-10-26 01:12:35 -0700

    Use media player

--- components/testimonial-detail.tsx
+++ components/testimonial-detail.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { Button } from "@/components/ui/button";
 import { format, formatDistance, formatRelative, subDays } from "date-fns";
 import { Spinner } from "@/components/ui/spinner";
+import AudioPlayer from "./media/audio-player";
+import VideoPlayer from "./media/video-player";
 
 type Props = {
   id: Id<"testimonials">;
@@ -65,10 +67,10 @@ export default function TestimonialDetail({ id }: Props) {
     <div className="flex flex-col gap-8">
       <h1 className="text-2xl font-bold">{testimonial.name}'s Testimonial</h1>
       {testimonial.mediaUrl && testimonial.media_type == "audio" && (
-        <audio className="w-full" controls src={testimonial.mediaUrl} />
+        <AudioPlayer src={testimonial.mediaUrl} />
       )}
       {testimonial.mediaUrl && testimonial.media_type == "video" && (
-        <video className="w-full" controls src={testimonial.mediaUrl} />
+        <VideoPlayer src={testimonial.mediaUrl} preload="auto" />
       )}
       <div className="flex gap-2">
         {testimonial.mediaUrl && (

1bc60fc88479ff38217314db2979b21f4aec7840
Viet Hung Nguyen <vhn@sfu.ca>
2025-10-25 19:42:49 -0700

    Video recorder on mobile

--- components/recorder/mobile-video-recorder.tsx
+++ components/recorder/mobile-video-recorder.tsx
@@ -1,13 +1,68 @@
+"use client";
+
+import { useRef } from "react";
 import { Button } from "../ui/button";
+import { useController } from "react-hook-form";
+import { Testimonial } from "@/lib/schema";
+import { Video } from "lucide-react";
 
-type Props = {
-  onRecordingComplete: (videoFile?: File) => void;
-};
+export default function MobileVideoRecorder() {
+  const { field } = useController<Testimonial>({
+    name: "mediaFile",
+  });
+  const inputRef = useRef<HTMLInputElement>(null);
 
-export default function MobileVideoRecorder({ onRecordingComplete }: Props) {
   return (
     <div className="flex flex-col p-4 border items-center rounded-lg gap-4 w-full">
-      <Button>Start Recording</Button>
+      <input
+        capture="user"
+        type="file"
+        accept="video/*"
+        className="hidden"
+        ref={(input) => {
+          field.ref(input);
+          inputRef.current = input;
+        }}
+        name={field.name}
+        onChange={(e) => {
+          e.preventDefault();
+          const file = e.target.files?.[0];
+          field.onChange(file);
+        }}
+      />
+
+      {/* Video Preview */}
+      {field.value && field.value instanceof File && (
+        <video
+          controls
+          src={URL.createObjectURL(field.value)}
+          className="w-full"
+        />
+      )}
+
+      {/* Controllers */}
+      <div className="flex items-center gap-3">
+        <Button
+          size="icon"
+          className="size-12 rounded-full"
+          type="button"
+          onClick={() => inputRef.current?.click()}
+        >
+          <Video className="size-6" />
+        </Button>
+        {field.value && (
+          <Button
+            type="button"
+            size="sm"
+            variant="outline"
+            onClick={() => {
+              field.onChange(undefined);
+            }}
+          >
+            Clear
+          </Button>
+        )}
+      </div>
     </div>
   );
 }

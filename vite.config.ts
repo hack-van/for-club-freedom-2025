ebaad1dd125754af2ac02aa0ef3a2750210ba968
Viet Hung Nguyen <vhn@sfu.ca>
2025-10-31 10:18:42 -0700

    Set up tanstack

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+    tanstackStart({
+      srcDirectory: 'src', // This is the default
+      router: {
+        // Specifies the directory TanStack Router uses for your routes.
+        routesDirectory: 'routes', // Defaults to "routes", relative to srcDirectory
+      },
+    }),
+    viteReact(),
+  ],
+})

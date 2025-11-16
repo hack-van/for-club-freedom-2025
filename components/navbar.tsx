3b08bbd5cdeac879712d91af522470bbb42b1da0
nicktsan <nicktsang.manhay@gmail.com>
2025-11-15 19:12:41 -0800

    adding conditionally rendered testimonial link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button";
 import { Authenticated, Unauthenticated, useQuery } from "convex/react";
 import { SignOutButton } from "@/components/auth/signout-button";
 import { api } from "@/convex/_generated/api";
+import { isModOrAdmin } from "@/convex/lib/permissions";
 
 export default function Navbar() {
   const user = useQuery(api.auth.getCurrentUser);
@@ -17,6 +18,15 @@ export default function Navbar() {
             alt="city-reach-logo"
           ></img>
         </Link>
+        {isModOrAdmin(user?.role) && (
+          <Authenticated>
+            <div className="flex items-center gap-4">
+              <Link href="/testimonials" className="ml-4">
+                <Button className="cursor-pointer">Testimonials</Button>
+              </Link>
+            </div>
+          </Authenticated>
+        )}
       </div>
       <Unauthenticated>
         <Link href="/signin" className="ml-4">

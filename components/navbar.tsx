67317b5eebcc1c0f46c3ba140f8569a298d72603
Viet Hung Nguyen <vhn@sfu.ca>
2025-11-18 15:39:56 -0800

    User menu dropdown

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,10 +3,10 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Authenticated, Unauthenticated, useQuery } from "convex/react";
-import { SignOutButton } from "@/components/auth/sign-out-button";
 import { api } from "@/convex/_generated/api";
 import { isModOrAdmin } from "@/convex/lib/permissions";
 import Logo from "./logo";
+import UserDropDown from "./user-dropdown";
 
 export default function Navbar() {
   const user = useQuery(api.auth.getCurrentUser);
@@ -18,7 +18,7 @@ export default function Navbar() {
         {isModOrAdmin(user?.role) && (
           <Authenticated>
             <div className="flex items-center gap-4">
-              <Button className="cursor-pointer" asChild>
+              <Button variant="link" className="cursor-pointer" asChild>
                 <Link href="/testimonials">Testimonials</Link>
               </Button>
             </div>
@@ -26,17 +26,12 @@ export default function Navbar() {
         )}
       </div>
       <Unauthenticated>
-        <Link href="/sign-in" className="ml-4">
-          <Button>Sign in</Button>
-        </Link>
+        <Button asChild>
+          <Link href="/sign-in">Sign in</Link>
+        </Button>
       </Unauthenticated>
       <Authenticated>
-        <div className="flex items-center gap-4">
-          Hello, {user?.email} ({user?.role})
-          <Link href="/sign-out" className="ml-4">
-            <SignOutButton />
-          </Link>
-        </div>
+        <UserDropDown />
       </Authenticated>
     </header>
   );
---
+++ components/user-dropdown.tsx
@@ -0,0 +1,50 @@
+import { api } from "@/convex/_generated/api";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useQuery } from "convex/react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { UserRoundIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+export default function UserDropDown() {
+  const user = useQuery(api.auth.getCurrentUser);
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+    router.push("/sign-in");
+  };
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarFallback>
+            <UserRoundIcon size={16} />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end">
+        <DropdownMenuLabel>
+          {user.name} ({user.role})
+        </DropdownMenuLabel>
+        <DropdownMenuLabel>
+          <span className="text-sm text-muted-foreground">{user.email}</span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleSignOut}>Sign out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}

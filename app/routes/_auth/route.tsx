7f382d1537a216933ddb3a00110f72c036bc27bf
Viet Hung Nguyen <vhn@sfu.ca>
2025-11-19 00:30:36 -0800

    Protected routes

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -1,9 +1,23 @@
 import AuthLayout from "@/components/layouts/auth-layout";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { authClient } from "@/lib/auth-client";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import { Unauthenticated } from "convex/react";
 
 export const Route = createFileRoute("/_auth")({
   component: RouteComponent,
+  beforeLoad: async () => {
+    const { data, error } = await authClient.getSession();
+    if (error) {
+      throw redirect({
+        to: "/",
+      });
+    }
+    if (data?.user) {
+      throw redirect({
+        to: "/testimonials",
+      });
+    }
+  },
 });
 
 function RouteComponent() {
